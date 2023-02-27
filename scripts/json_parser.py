6d17bc4e0c602a8297b5ffee0206179a8225d326
Pablo Orviz <orviz@ifca.unican.es>
2023-02-27 16:20:34 +0100

    Option that returns release info

--- scripts/json_parser.py
+++ scripts/json_parser.py
@@ -21,23 +21,27 @@ if __name__ == '__main__':
         print('Usage:', sys.argv[0],
               '<product_metadata_file> (relative path from root repo) <1> optional')
         print('1 - creates a new json file ready for release/publishing, named release.json')
+        print('2 - returns release info from the product\'s json file')
         sys.exit(1)
 
     print(f'Get product metadata file: {sys.argv[1]}')
     ev = Config().getconf(sys.argv[1])
 
-    if (len(sys.argv) == 3) and (sys.argv[2] == '1'):
+    if len(sys.argv) == 3:
         # Option to create new json for release/publishing
         (dst_type, dst_version, platform, arch) = utils.get_info_json(ev['json_file'])
 
-        # full uri is repo_uri_path/rel_uripath ->
-        # https://nexusrepoegi.a.incd.pt/repository/umd/5/<OPERATING_SYSTEM>/testing/<ARCH>
-        rel_uripath = dst_type + '/' + dst_version + '/' + platform + '/' + 'release' + '/' + arch
-        uri_path = ev['repo_uri_path'] + '/' + rel_uripath
-        new_json = utils.create_json(ev['json_file'], uri_path)
-        new_file = ev['tmp_base_dir'] + '/' + 'release.json'
-        utils.write_new_json(new_json, new_file)
-        sys.exit(0)
+        if sys.argv[2] == '1':
+            # full uri is repo_uri_path/rel_uripath ->
+            # https://nexusrepoegi.a.incd.pt/repository/umd/5/<OPERATING_SYSTEM>/testing/<ARCH>
+            rel_uripath = dst_type + '/' + dst_version + '/' + platform + '/' + 'release' + '/' + arch
+            uri_path = ev['repo_uri_path'] + '/' + rel_uripath
+            new_json = utils.create_json(ev['json_file'], uri_path)
+            new_file = ev['tmp_base_dir'] + '/' + 'release.json'
+            utils.write_new_json(new_json, new_file)
+            sys.exit(0)
+        elif sys.argv[2] == '2':
+            print(dst_type, dst_version, platform, arch)
 
 
     # Directory containing the json files
